bd92313afd90c5337e2b4352244bd8f9e0b487f5
Pratik Narola <35926958+Pratiknarola@users.noreply.github.com>
2018-12-24 18:50:54 +0530

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -1,10 +1,6 @@
 import random
 import string
 def pass_gen(choice):
-    weak_list = [12345, 'admin', 'password', 'iloveyou', 'qwerty', 'asdfg', '12345678', 'aaaaaa', '111111', 'letmein',
-                 'football', 'welcome','incorrect', 'login', 'passw0rd', 'hello', 'freedom', 'qazwsx', 'passwod1',
-                 '123123', 'computer', '121212', 'amanda', 'charlie', 'maggie', 'test', 'p@ssword', 'dragon', 'starwars',
-				 'whatever', 'trustno1', '654321', 'robert', 'matthew', '1qaz2wsx', 'sunshine', 'meandyou', 'admin123', 'name@123', '1q2w3e']
     if choice == "1":
         return random.choice(list(open('files/password.txt', 'r')))
     elif choice == '2':
